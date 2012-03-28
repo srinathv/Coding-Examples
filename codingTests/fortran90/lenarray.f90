c32073017ed7897a0d73412342325424b103f27c
Srinath Vadlamani <srinath@txcorp.com>
2012-03-28 16:45:32 -0600

    MOre fortran stuff

--- codingTests/fortran90/lenarray.f90
+++ codingTests/fortran90/lenarray.f90
@@ -3,8 +3,10 @@
       program main
 
       integer, DIMENSION(5) :: array
+      integer, DIMENSION(5,6) :: array2
 
 
       write(*,*) SIZE(array)
+      write(*,*) SIZE(array2)
 
       end program
