3893c39686301503652dd9b6948515961c3e7927
Srinath Vadlamani <srinath@txcorp.com>
2012-04-22 12:37:35 -0600

    Another example, user input followed return

---
+++ cin1.cpp
@@ -0,0 +1,13 @@
+
+/*program to show simple cin use */
+
+#include <iostream>
+
+  int main () {
+
+    int thisisanumber ;
+
+    std::cout << "Please enter a number: ";
+    std::cin >> thisisanumber ;
+    std::cout << " You entered: " << thisisanumber << "\n";
+  }
