dba7b5b424f5940d62205ba7abf3bc3085df5163
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2012-05-17 15:53:24 -0600

    Array scoping

---
+++ codingTests/fortran90/allocTrick.f90
@@ -0,0 +1,28 @@
+
+
+  Program allocTrick
+
+  ! I want to get all the info from the allocated array itself
+
+  implicit none
+
+
+  integer :: n1=1,n2=2,n3=3
+  integer :: ranks(1),i
+  real, allocatable :: array(:,:,:)
+  integer, allocatable :: dims(:)
+
+
+
+
+  allocate(array(n1,n2,n3))
+
+  ranks=shape(shape(array))
+  write(*,*) "ranks= ", ranks
+  allocate(dims(ranks(1)))
+  dims=shape(array)
+  write(*,*) "shape of array = ", dims
+  write(*,*) "dimension of array = ", ranks
+  
+
+  End Program allocTrick
