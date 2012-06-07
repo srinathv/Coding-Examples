78cd067a0a1e7339ad2668cc49e175452a64cba2
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2012-06-06 23:01:06 -0600

    Chapter 5 examples

---
+++ jumpingIntoC++/for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main () {
+    // The loop goes while i < 10, increasing i by one each loop
+  for ( int i = 0 ; i < 10 ; i++ ) {
+    // The condition is checked each loop
+    // When i equals 10, then the loop breaks.
+    std::cout << i << " squared is " << i * i << std::endl ;
+  }
+}
+     
---
+++ jumpingIntoC++/while.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main () {
+  int i = 0; // always declare variables
+  while (i < 10 ) {
+    std::cout << i << '\n';
+    i++; //increment i
+  }
+}
