1c38a7f685529b6a7b00e2069f6633f64c7e3470
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2013-02-13 09:31:16 -0700

    need to keep track of configs

---
+++ configs/openmpi_pgi_macosx_i5.sh
@@ -0,0 +1,10 @@
+./configure \
+  --prefix=/Users/srinathv/software/ompi/pgi \
+  FC=pgfortran \
+  FCFLAGS='-tp= nehalem-64' \
+  F77=pgfortran \
+  FFLAGS='-tp= nehalem-64' \
+  CC=pgcc \
+  CFLAGS='-tp= nehalem-64' \
+  CXX=pgcpp \
+  CXXFLAGS='-tp= nehalem-64' 
