226fc001a3164c5dd35f9022aa3184019aa94008
Srinath Vadlamani <srinathv@ucar.edu>
2013-04-11 15:35:08 -0600

    simple script to show sequence

--- performance/vpuGlfops.F90
+++ performance/vpuGlfops.F90
@@ -29,7 +29,8 @@
   integer, parameter :: vpuLength = 4
 #endif
 
-  integer :: i,j,k,fmaNumFlop,dubNumFlop
+  integer :: i,j,k
+  real :: fmaNumFlop,dubNumFlop
 
 
   real, dimension(nThreads*vpuLength) :: a,b,x,y,z
@@ -62,20 +63,23 @@
       call cpu_time(fmaStartTime)
 #endif
 
+  do k = 1, 100000
   do i = 1, it
-!omp parallel do
+!omp parallel do shared(z) private(j)
     do j=1,nThreads*vpuLength
   
       z(j)=a(j)*x(j)+b(j)
     enddo
 !omp end parallel do
   enddo 
-!end time
+  enddo 
 #if defined(HAVE_OMP)
       fmaEndTime = omp_get_wtime()
 #else
       call cpu_time(fmaEndTime)
 #endif
+
+    write(*,*) "fma maxval(z) =", maxval(z) ,"should be 2"
 !---------------------------------------------
 
 !reinitialize arrays
@@ -92,13 +96,15 @@
 #endif
 
 !add and mults
+ do k = 1, 100000
  do i = 1, it
-!omp parallel do
+!omp parallel do shared(z) private(j)
     do j=1,nThreads*vpuLength
   
       z(j)=a(j)+x(j)+b(j)
     enddo
 !omp end parallel do
+  enddo 
   enddo 
 #if defined(HAVE_OMP)
       dubEndTime = omp_get_wtime()
@@ -106,19 +112,20 @@
       call cpu_time(dubEndTime)
 #endif
 
+    write(*,*) "dub maxval(z) =", maxval(z) ,"should be 3"
 
 !report times and flops
 write(*,*) " fma time = ", fmaEndTime-fmaStartTime
 write(*,*) " dub time = ", dubEndTime-dubStartTime
 
 !total number of flops
-fmaNumFlop=it*nThreads*vpuLength*2
-dubNumFlop=it*nThreads*vpuLength*2
+fmaNumFlop=real(it)*100000*real(nThreads)*real(vpuLength)*2
+dubNumFlop=fmaNumFlop
 
-write(*,*) " FmaNumFlop = ",fmaNumFlop
-write(*,*) " dubNumFlop = ",dubNumFlop
+write(*,*) " FmaNumFlop = ",real(fmaNumFlop)
+write(*,*) " dubNumFlop = ",real(dubNumFlop)
 
-write(*,*) "Gflops for FMA = ", (fmaNumFlop * 10E-9)/(fmaEndTime-fmaStartTime)
-write(*,*) "Gflops for dub = ", (dubNumFlop * 10E-9)/(dubEndTime-dubStartTime)
+write(*,*) "Gflops for FMA = ", (real(fmaNumFlop) * 1E-9)/(fmaEndTime-fmaStartTime)
+write(*,*) "Gflops for dub = ", (real(dubNumFlop) * 1E-9)/(dubEndTime-dubStartTime)
 
 end program testVpu
---
+++ scripting/sequence.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Bash version ${BASH_VERSION}..."
+for i in $(seq 1 2 20) 
+  do
+     echo "Welcome $i times"
+ done
