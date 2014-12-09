ee622e326d0356678ee1b6312b0282c8f56664f2
Srinath Vadlamani <srinathv@ucar.edu>
2014-12-09 14:19:05 -0700

    simple way to chase down paths

---
+++ codingTests/python/pathHunting.py
@@ -0,0 +1,20 @@
+import os
+
+print("Path at terminal when executing this file")
+print(os.getcwd() + "\n")
+
+print("This file path, relative to os.getcwd()")
+print(__file__ + "\n")
+
+print("This file full path (following symlinks)")
+full_path = os.path.realpath(__file__)
+print(full_path + "\n")
+
+print("This file directory and name")
+path, file = os.path.split(full_path)
+print(path + ' --> ' + file + "\n")
+
+print("This file directory only")
+print(os.path.dirname(full_path))
+
+print path.split('/')[-1]
