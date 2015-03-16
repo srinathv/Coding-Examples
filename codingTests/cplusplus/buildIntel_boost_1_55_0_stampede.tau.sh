a35c72d8c626da34a8a8b4d0d3f49ff4e1167f62
Srinath Vadlamani <srinathv@ucar.edu>
2015-03-16 12:46:45 -0500

    trying to get /opt/apps/intel/13/composer_xe_2013_sp1.1.106/ipp/include:/opt/apps/intel/13/composer_xe_2013_sp1.1.106/mkl/include:/opt/apps/cuda/6.0/include to work

--- codingTests/cplusplus/buildIntel_boost_1_55_0_stampede.tau.sh
+++ codingTests/cplusplus/buildIntel_boost_1_55_0_stampede.tau.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
+BOOST_INCLUDE=-I/scratch/02463/srinathv/TEST/Gravity/boost_1_55_0/install/include/
 export TAU_MAKEFILE=/work/02463/srinathv/tau2/x86_64/lib/Makefile.tau-intelImpiCuda.aac-icpc-papi-mpi-cupti-pdt
+export TAU_OPTIONS="-optLinkOnly -optTauIncludes=${BOOST_INCLUDE}"
 
-tau_cxx.sh testVec.cpp -o testVec -O2 -fPIC /scratch/02463/srinathv/TEST/Gravity/boost_1_55_0/install/lib/libboost_system.a /scratch/02463/srinathv/TEST/Gravity/boost_1_55_0/install/lib/libboost_timer.a  /scratch/02463/srinathv/TEST/Gravity/boost_1_55_0/install/lib/libboost_chrono.a /scratch/02463/srinathv/TEST/Gravity/boost_1_55_0/install/lib/libboost_thread.a -I/scratch/02463/srinathv/TEST/Gravity/boost_1_55_0/install/include -lrt
+tau_cxx.sh testVec.cpp -o testVec_tau -O2 -fPIC -I/scratch/02463/srinathv/TEST/Gravity/boost_1_55_0/install/include/ /scratch/02463/srinathv/TEST/Gravity/boost_1_55_0/install/lib/libboost_system.a /scratch/02463/srinathv/TEST/Gravity/boost_1_55_0/install/lib/libboost_timer.a  /scratch/02463/srinathv/TEST/Gravity/boost_1_55_0/install/lib/libboost_chrono.a /scratch/02463/srinathv/TEST/Gravity/boost_1_55_0/install/lib/libboost_thread.a -lrt
