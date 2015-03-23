e9df397870ad31aed5be7e4aec20814fbc7f3434
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-03-23 16:36:00 -0600

    run now takes executable arguement

--- codingTests/cplusplus/run100.sh
+++ codingTests/cplusplus/run100.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 
+
 trials=100
         rm -f testVec.out
  for i in `seq 1 ${trials}`;
         do
  #               echo $i
-	./testVec >> testVec.out
+	./${1} >> testVec.out
 	sleep 1
         done 
