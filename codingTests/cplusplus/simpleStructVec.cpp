79a388b30d59ea487af1e67b45911d05f243d274
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-04-27 14:34:16 -0600

    shows how to access vector of structs

--- codingTests/cplusplus/simpleStructVec.cpp
+++ codingTests/cplusplus/simpleStructVec.cpp
@@ -1,17 +1,27 @@
 #include <vector>
 #include <iostream>
 
+struct direction {
+  float xhat;
+  float yhat;
+  float zhat;
+  };
+
+
 struct test {
-    std::vector<int> vec;
-    std::vector<float> floatVec(3); //does not compile
+    std::vector<direction> vec;
 };
 
 //std::vector<float> floatVec(3);  //compiles
 
 test mytest;
 
+direction thisway = {1,2,3};
 void foo() {
-    mytest.vec.push_back(3);
+  for (int i= 0; i< 201; i++)
+  {
+   mytest.vec.push_back(thisway);
+  }
 }
 
 int main(int argc, char** argv)
@@ -19,7 +29,11 @@ int main(int argc, char** argv)
    foo();
    if (!mytest.vec.empty())  // it's always good to test container is empty or not
    {
-     std::cout << "Vector Element" << mytest.vec[0] << std::endl;
+     std::cout << "Vector Element 200 xhat " << mytest.vec[200].xhat << std::endl;
+     std::cout << "Vector Element 200 zhat " << mytest.vec[200].zhat << std::endl;
+     std::cout << "Vector capacity " << mytest.vec.capacity() << std::endl;
+     std::cout << "Vector Element 255 zhat " << mytest.vec[255].zhat << std::endl;
+     std::cout << "Vector Element 256 zhat " << mytest.vec[256].zhat << std::endl;
    }
    return 0;
 }
