63a6c90f8b58c5b6d3e218911d00fa2997553a0b
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-04-27 21:19:33 -0600

    inner vector assigned via function

--- codingTests/cplusplus/simpleStructVec.cpp
+++ codingTests/cplusplus/simpleStructVec.cpp
@@ -11,41 +11,41 @@ struct direction {
 struct test {
     std::vector<direction> vec;
     int length;
-};
+    };
 
 //std::vector<float> floatVec(3);  //compiles
 
-test mytest;
-std::vector<test> myVecTest;
-
-direction thisway = {1,2,3};
-
-
-void foo() {
+void foo(test &directionVec, direction &whichway) {
   for (int i= 0; i< 201; i++)
   {
-   mytest.vec.push_back(thisway);
+   directionVec.vec.push_back(whichway);
+   directionVec.length =2 ;
   }
 }
 
 
-void vecFoo() {
-  for (int i = 0 ; i < 2; i++)
-  {
-//    myVecTest.push_back(test()); //initialize first element which is a struct
-//    myVecTest[0].length=2;
-//    myVecTest[0].vec.push_back(thisway);
-//    myVecTest.push_back(test()); // add second element
-//    myVecTest[1].length=3;
-//    myVecTest[1].vec.push_back(thisway);
-    myVecTest.push_back(test()); //initialize first element which is a struct
-    myVecTest[i].length=2;
-    myVecTest[i].vec.push_back(thisway);
-  }
-}
+
+
+
+
+//void vecFoo(std::vector<direction> *vecOfVecs, test *insideVec, direction *whichway) {
+//  for (int i = 0 ; i < 2; i++)
+//  {
+//      vecOfVecs.push_back(&insideVec);
+//      foo(&vecOfVecs[i]);
+//  }
+//}
+
+
 int main(int argc, char** argv)
 {
-   foo();
+
+   test mytest;
+   std::vector<test> myVecTest;
+
+   direction thisway = {1,2,3};
+
+   foo(mytest,thisway);
    if (!mytest.vec.empty())  // it's always good to test container is empty or not
    {
      std::cout << "Vector Element 200 xhat " << mytest.vec[200].xhat << std::endl;
@@ -55,10 +55,10 @@ int main(int argc, char** argv)
      std::cout << "Vector Element 256 zhat " << mytest.vec[256].zhat << std::endl;
    }
 
-   vecFoo();
-   if (!myVecTest.empty())
-   {
-    std::cout << "myVecTest " << myVecTest[1].vec[0].xhat << std::endl;
-   }
+//   vecFoo(myVecTest,mytest,direction);
+//   if (!myVecTest.empty())
+//   {
+//    std::cout << "myVecTest " << myVecTest[1].vec[0].xhat << std::endl;
+//   }
    return 0;
 }
