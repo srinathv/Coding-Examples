07a5bea5c46b733a6188b754b62c753075af3f5a
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-04-28 15:44:33 -0600

    C&R: now makes a vector of N skinny rays

--- codingTests/cplusplus/testStructVec.cpp
+++ codingTests/cplusplus/testStructVec.cpp
@@ -1,13 +1,11 @@
 
 #include <iostream>
-#include <boost/container/vector.hpp>
-#include <boost/timer/timer.hpp>
 #include <vector>
-#ifdef USE_TAU
-#include <TAU.h>
-#endif
-#ifdef USE_TBB
-#include "tbb/concurrent_vector.h"
+
+#ifdef SET_N
+  int N=SET_N;
+#else
+  int N=1000000;
 #endif
 
 
@@ -47,103 +45,35 @@
   int pixel ;
   };
 
-  direction normal = {1.1,3.45,2.2};
+//could have a function that grows the direction vector but no need for now.
 
 
-  manyFatArrays(fat array,[
+  void makeVecOfSkinny(std::vector<skinny> &vecOfSkinny, skinny &singleRay, direction &whichway,
+                float origin[3], float &distance ) {
+    for (int i = 0 ; i < N+1; i++)
+    {
+    vecOfSkinny.push_back(singleRay);
+    vecOfSkinny[i].vecDirection.push_back(whichway);
+    vecOfSkinny[i].origin[0] = origin[0];
+    vecOfSkinny[i].origin[1] = origin[1];
+    vecOfSkinny[i].origin[2] = origin[2];
+    vecOfSkinny[i].distance = distance;
+    }
+}
 
 int main(){
-#ifdef USE_TAU
-  TAU_START("main");
-  TAU_PROFILE_SET_NODE(0);
-#endif
-#ifdef SET_N
-  int N=SET_N;
-#else
-  int N=1000000;
-#endif
-
-#ifdef VEC
 
 
-  std::vector<skinny> v1;
-  std::cout << "This is std::vector push_back time" << std::endl;
+  direction normal = {1.1,3.45,2.2};
+  float myOrigin[3] = { 20.1, 22.2, 33.3};
+  float distance = 4533.23422;
 
-  {
-    boost::timer::auto_cpu_timer t;
-#ifdef USE_TAU
-  TAU_START("std::vector.push_back loop");
-#endif
-  {
-  for (int i = 0; i < N; ++i)
-    {
-        v1.push_back(float(i));
-    }
-  }
-  }
-#ifdef USE_TAU
-  TAU_STOP("std::vector.push_back loop");
-#endif
-#ifdef USE_TAU
-  TAU_START("std::vector.pop_back loop");
-#endif
-  std::cout << "This is std::vector pop_back time" << std::endl;
-  {
-  boost::timer::auto_cpu_timer t;
-  {
-  for (int i = 0; i < N; ++i)
-    {
-        v1.pop_back();
-    }
-  }
-#ifdef USE_TAU
-  TAU_STOP("std::vector.pop_back loop");
-#endif
+  std::vector<skinny> myVec;
+  skinny singleRay;
 
-  }
-#endif
+  makeVecOfSkinny(myVec, singleRay, normal, myOrigin, distance);
 
 
-#ifdef BOOST_VEC
-  std::cout << "This is boost::container::vector push_back time" << std::endl;
-  {
-    boost::timer::auto_cpu_timer t;
-    boost::container::vector<float> v3;
-#ifdef USE_TAU
-  TAU_START("boost::container::vector.push_back loop");
-#endif
-    for (int i = 0; i < N; ++i)
-    {
-        v3.push_back(float(i));
-    }
-#ifdef USE_TAU
-  TAU_STOP("boost::container::vector.push_back loop");
-#endif
-  }
-#endif
-
-
-#ifdef DEQ
-  std::cout << "This is std::deque push_back time" << std::endl;
-  {
-    boost::timer::auto_cpu_timer t;
-    std::deque <float> v4;
-#ifdef USE_TAU
-  TAU_START(" std::deque.push_back loop");
-#endif
-    for (int i = 0; i < N; ++i)
-    {
-        v4.push_back(float(i));
-    }
-#ifdef USE_TAU
-  TAU_STOP(" std::deque.push_back loop");
-#endif
-  }
-#endif
-
-#ifdef USE_TAU
-  TAU_STOP("main");
-#endif
 
 	return 0;
 }
