e10ec5570de8d02bb0d160272c8d957483d9ee16
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-07-15 09:32:36 -0600

    a test for the elf library

---
+++ codingTests/testConfigures/elf_begin_test.c
@@ -0,0 +1,98 @@
+/* confdefs.h.  */
+#define PACKAGE_NAME "threadspotter"
+#define PACKAGE_TARNAME "threadspotter"
+#define PACKAGE_VERSION "2012.1.1"
+#define PACKAGE_STRING "threadspotter 2012.1.1"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE "threadspotter"
+#define VERSION "2012.1.1"
+#define LITMUS_VERSION_MAJOR  2
+#define LITMUS_VERSION_MINOR  0
+#define LITMUS_VERSION_PATCH  0
+#define LITMUS_VERSION_RELEASE  1
+#define LITMUS_VERSION  "2.0.0"
+#define HAVE_LIBNSL 1
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_ALLOCA_H 1
+#define HAVE_ALLOCA 1
+#define HAVE_DIRENT_H 1
+#define STDC_HEADERS 1
+#define HAVE_SYS_WAIT_H 1
+#define HAVE_FCNTL_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_LIBINTL_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_NETINET_IN_H 1
+#define HAVE_NL_TYPES_H 1
+#define HAVE_STDDEF_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_SYS_SOCKET_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE__BOOL 1
+#define HAVE_STDBOOL_H 1
+#define TIME_WITH_SYS_TIME 1
+#define HAVE_PTRDIFF_T 1
+#define HAVE_FORK 1
+#define HAVE_VFORK 1
+#define HAVE_WORKING_VFORK 1
+#define HAVE_WORKING_FORK 1
+#define HAVE_STDLIB_H 1
+#define HAVE_MALLOC 1
+#define HAVE_STDLIB_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_GETPAGESIZE 1
+#define HAVE_MMAP 1
+#define HAVE_STDLIB_H 1
+#define HAVE_REALLOC 1
+#define LSTAT_FOLLOWS_SLASHED_SYMLINK 1
+#define HAVE_VPRINTF 1
+#define HAVE_ATEXIT 1
+#define HAVE_FLOOR 1
+#define HAVE_FTRUNCATE 1
+#define HAVE_GETCWD 1
+#define HAVE_GETPAGESIZE 1
+#define HAVE_GETTIMEOFDAY 1
+#define HAVE_MEMCHR 1
+#define HAVE_MEMMOVE 1
+#define HAVE_MEMSET 1
+#define HAVE_MUNMAP 1
+#define HAVE_POW 1
+#define HAVE_SETLOCALE 1
+#define HAVE_STRCHR 1
+#define HAVE_STRDUP 1
+#define HAVE_STRERROR 1
+#define HAVE_STRRCHR 1
+#define HAVE_STRSTR 1
+#define HAVE_STRTOL 1
+#define HAVE_STRTOUL 1
+/* end confdefs.h.  */
+
+/* Override any GCC internal prototype to avoid an error.
+   Use char because int might match the return type of a GCC
+   builtin and then its argument prototype would still apply.  */
+#ifdef __cplusplus
+extern "C"
+#endif
+char elf_begin ();
+int
+main ()
+{
+return elf_begin ();
+  ;
+  return 0;
+}
