5b98921f221b904ff83a3d86b6cc34986d9b3a47
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2016-04-26 19:45:12 -0600

    new code to thread level

---
+++ codingTests/Mpi_stuff/mpi_thread_support.c
@@ -0,0 +1,18 @@
+//from llnl mpi tutorial page
+#include "mpi.h"
+#include <stdio.h>
+int int main(int argc, char const *argv[]) {
+  int provided, claimed;
+  /*** Selec on of the following
+  MPI_Init_thread( 0, 0, MPI_THREAD_SINGLE, &provided);
+  MPI_Init_thread(0,0, MPI_THREAD_FUNNELED, &provided);
+  MPI_Init_thread(0,0,MPI_THREAD_SERIALIZED, &provided);
+  MPI_Init_thread(0,0, MPI_THREAD_MULTIPLE, &provided);
+  ***/
+
+  MPI_Init_thread (0,0, MPI_THREAD_MULTIPLE, &provided);
+  MPI_Query_thread (&claimed);
+    printf("Query thread level = %d   Init_thread level = %d\n", );
+  MPI_Finalize();
+  return 0;
+}
