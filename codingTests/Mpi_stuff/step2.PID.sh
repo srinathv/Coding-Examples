728b957597ef48d4acfa67f4bfede118f1ca1b27
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2016-05-16 20:55:36 -0600

    these show different PID per line of multiple scripts

---
+++ codingTests/Mpi_stuff/step2.PID.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+# this is to test to append a file that has the same PID
+
+echo "Step2 added this line for PID =", $$ >> steps.$$.txt
---
+++ codingTests/Mpi_stuff/steps.PID.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+#run the step<>.PID.sh files
+
+
+step1.PID.sh
+step2.PID.sh
