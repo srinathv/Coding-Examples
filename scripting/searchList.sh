90d2bca69334266f0288a3f6eb68415a121b75bf
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2016-07-20 10:01:37 -0600

    bashing a list collector

--- scripting/searchList.sh
+++ scripting/searchList.sh
@@ -1,1 +1,6 @@
-!# /bin/env bash
+#!/usr/bin/env bash
+
+#want a script that will
+#1)produce a number of files with <foo>.<n>.test
+#2)gather list of n's
+#3)run an operation over the list of n' (cp or something)
