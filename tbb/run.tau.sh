c8bf8567a406e89b2aa00c9ec97e379300588fa7
Srinath Vadlamani <srinathv@ucar.edu>
2017-02-07 16:10:53 -0600

    added the threads one

--- tbb/run.tau.sh
+++ tbb/run.tau.sh
@@ -1,3 +1,4 @@
 
 #TAU_PROFILE_FORMAT=merged
-ibrun tau_exec -T knl_17,icpc,papi,mpi,tbb ./mpi_tbb_mm.tauLink.knl.exe
+#ibrun tau_exec -T knl_17,icpc,papi,mpi,tbb ./mpi_tbb_mm.tauLink.knl.exe
+ibrun tau_exec -T knl_17,icpc,papi,mpi,tbb ./mpi_tbb_mm.argsThread.knl.exe -threads 64
