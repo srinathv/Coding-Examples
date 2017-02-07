c2acae6390049487b34860d8548c0f18fe5406d6
Srinath Vadlamani <srinathv@ucar.edu>
2017-02-07 16:14:38 -0600

    seems to be running ok

--- tbb/run.tau.sh
+++ tbb/run.tau.sh
@@ -1,4 +1,4 @@
 
 #TAU_PROFILE_FORMAT=merged
 #ibrun tau_exec -T knl_17,icpc,papi,mpi,tbb ./mpi_tbb_mm.tauLink.knl.exe
-ibrun tau_exec -T knl_17,icpc,papi,mpi,tbb ./mpi_tbb_mm.argsThread.knl.exe -threads 64
+ibrun -n 2 -o 0 tau_exec -T knl_17,icpc,papi,mpi,tbb ./mpi_tbb_mm.argsThread.knl.exe -threads 12
