6affd9caa02ce34db4475f0975b4b0d05b226832
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2017-02-07 09:29:03 -0700

    added command line arg for thread count

--- tbb/simpleTbb.cpp
+++ tbb/simpleTbb.cpp
@@ -14,10 +14,30 @@ struct mytask {
   size_t _n;
 };
 
+
+/*********************  MAIN ********************/
 int main(int,char**) {
 
-  //tbb::task_scheduler_init init;  // Automatic number of threads
-  tbb::task_scheduler_init init(tbb::task_scheduler_init::default_num_threads());  // Explicit number of threads
+
+
+#if defined (__LIKE_GRAVIT)
+  cmd.parse(argc, argv);
+
+tbb::task_scheduler_init* init;
+if (!cmd.isSet("threads")) {
+  init = new tbb::task_scheduler_init(std::thread::hardware_concurrency());
+  std::cout << "Initialized GraviT with " << std::thread::hardware_concurrency() <<
+  " threads..."<< std::endl;
+} else {
+  init = new tbb::task_scheduler_init(cmd.get<int>("threads"));
+  std::cout << "Initialized GraviT with " << cmd.get<int>("threads") <<
+    " threads..."<< std::endl;
+}
+#else
+//tbb::task_scheduler_init init;  // Automatic number of threads
+tbb::task_scheduler_init init(tbb::task_scheduler_init::default_num_threads());  // Explicit number of threads
+#endif
+
 
   std::vector<mytask> tasks;
   for (int i=0;i<1000;++i)
