f837fca53a55a36a6666411d347092ad71bce1f9
Srinath Vadlamani <srinathv@ucar.edu>
2017-02-13 17:40:19 -0600

    try the matmult

---
+++ tbb/matmul.cpp
@@ -0,0 +1,45 @@
+/* matrix-tbb.cpp */
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+
+using namespace tbb;
+
+const int size = 1000;
+
+float a[size][size];
+float b[size][size];
+float c[size][size];
+
+
+class Multiply
+{
+public:
+    void operator()(blocked_range<int> r) const {
+        for (int i = r.begin(); i != r.end(); ++i) {
+            for (int j = 0; j < size; ++j) {
+                for (int k = 0; k < size; ++k) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+    }
+};
+
+
+int main()
+{
+    // Initialize buffers.
+    //     for (int i = 0; i < size; ++i) {
+    //             for (int j = 0; j < size; ++j) {
+    //                         a[i][j] = (float)i + j;
+    //                                     b[i][j] = (float)i - j;
+    //                                                 c[i][j] = 0.0f;
+    //                                                         }
+    //                                                             }
+    //
+    //                                                                 // Compute matrix multiplication.
+    //                                                                     // C <- C + A x B
+    //                                                                         parallel_for(blocked_range<int>(0,size), Multiply());
+    //
+    //                                                                             return 0;
+    //                                                                             }G
