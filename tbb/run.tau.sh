cc74df4265abb19c72cda7f5df4c9c1b1ddec15d
Srinath Vadlamani <srinathv@ucar.edu>
2017-02-13 16:46:33 -0600

    tau link is not running w/o args

--- tbb/run.tau.sh
+++ tbb/run.tau.sh
@@ -1,4 +1,11 @@
 
 #TAU_PROFILE_FORMAT=merged
 #ibrun tau_exec -T knl_17,icpc,papi,mpi,tbb ./mpi_tbb_mm.tauLink.knl.exe
-ibrun -n 2 -o 0 tau_exec -T knl_17,icpc,papi,mpi,tbb ./mpi_tbb_mm.argsThread.knl.exe -threads 122
+export LD_LIBRARY_PATH=/opt/apps/papi/5.5.0/lib:$LD_LIBRARY_PATH
+if [ -z "$1" ]
+ then
+   ibrun -n 2 -o 0 tau_exec -T knl_17,icpc,papi,mpi,tbb ./mpi_tbb_mm.argsThread.knl.tau.exe -threads $1
+else
+   ibrun -n 2 -o 0 tau_exec -T knl_17,icpc,papi,mpi,tbb ./mpi_tbb_mm.argsThread.knl.tau.exe 
+fi
+
