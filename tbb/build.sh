2356a570f0b2fa50e9b53528d1391bd9f8de4445
Srinath Vadlamani <srinathv@ucar.edu>
2017-02-14 14:26:32 -0600

    added the default threads count to build list

--- tbb/build.sh
+++ tbb/build.sh
@@ -1,4 +1,5 @@
 
 mpiicpc -D__USE_TBB -D__LIKE_GRAVIT mpi_tbb_mm.cpp -tbb -std=c++0x -o mpi_tbb_mm.argsThread.knl.exe
+mpiicpc -D__USE_TBB mpi_tbb_mm.cpp -tbb -std=c++0x -o mpi_tbb_mm.knl.exe
 mpiicpc mpi_tbb_mm.cpp -std=c++0x -o mpi_mm.knl.exe
 icpc matmul.cpp -tbb -o matmul.exe
