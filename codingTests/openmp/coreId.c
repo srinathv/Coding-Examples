cda71a150b4108fdd468b37aad4e036e7609d802
Srinath Vadlamani <srinath@txcorp.com>
2017-10-11 16:09:59 -0700

    formating

--- codingTests/openmp/coreId.c
+++ codingTests/openmp/coreId.c
@@ -5,10 +5,10 @@
 int main() {
 #pragma omp parallel
       {
-                int thread_num = omp_get_thread_num();
-                        int cpu_num = sched_getcpu();
-                                printf("Thread %3d is running on CPU %3d\n", thread_num, cpu_num);
-                                    }
+       int thread_num = omp_get_thread_num();
+       int cpu_num = sched_getcpu();
+       printf("Thread %3d is running on CPU %3d\n", thread_num, cpu_num);
+      }
 
           return 0;
 }
