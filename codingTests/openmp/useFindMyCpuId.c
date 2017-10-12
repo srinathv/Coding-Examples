6794af80e4160479c7534360051cac958528107f
Srinath Vadlamani <srinath@txcorp.com>
2017-10-12 10:41:01 -0700

    C code calling findmycpu

--- codingTests/openmp/useFindMyCpuId.c
+++ codingTests/openmp/useFindMyCpuId.c
@@ -7,7 +7,7 @@ int main() {
       {
        int thread_num = omp_get_thread_num();
        int cpu_num = sched_getcpu();
-       int findmycpu_num = findmycpu();
+       int findmycpu_num = findmycpu_();
        printf("Thread %3d is running on CPU %3d\n", thread_num, cpu_num);
        printf("Thread %3d is running on myCPU %3d\n", thread_num, findmycpu_num);
       }
