ae45b8b25bce128efbb03895262ed2f2875c18f0
Srinath Vadlamani <srinath@txcorp.com>
2017-10-12 10:32:02 -0700

    starting to set up core identifier

---
+++ codingTests/openmp/useFindMyCpuId.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sched.h>
+#include <omp.h>
+
+int main() {
+#pragma omp parallel
+      {
+       int thread_num = omp_get_thread_num();
+       int cpu_num = sched_getcpu();
+       int findmycpu_num = findmycpu();
+       printf("Thread %3d is running on CPU %3d\n", thread_num, cpu_num);
+       printf("Thread %3d is running on myCPU %3d\n", thread_num, findmycpu_num);
+      }
+
+          return 0;
+}
