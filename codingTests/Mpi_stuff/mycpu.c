5f88cebc7ae593f23308599acce020ef9d619a50
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2017-10-13 22:16:36 -0600

    starting for mac os x bsd os

--- codingTests/Mpi_stuff/mycpu.c
+++ codingTests/Mpi_stuff/mycpu.c
@@ -2,6 +2,26 @@
 #include <sched.h>
 int sched_getcpu();
 
+#if defined (MAC_OS_X)
+#include <cpuid.h>
+
+#define CPUID(INFO, LEAF, SUBLEAF) __cpuid_count(LEAF, SUBLEAF, INFO[0], INFO[1], INFO[2], INFO[3])
+
+
+#define GETCPU(CPU) {                              \
+        uint32_t CPUInfo[4];                           \
+        CPUID(CPUInfo, 1, 0);                          \
+        /* CPUInfo[1] is EBX, bits 24-31 are APIC ID */ \
+        if ( (CPUInfo[3] & (1 << 9)) == 0) {           \
+          CPU = -1;  /* no APIC on chip */             \
+        }                                              \
+        else {                                         \
+          CPU = (unsigned)CPUInfo[1] >> 24;                    \
+        }                                              \
+        if (CPU < 0) CPU = 0;                          \
+      }
+#endif
+
 int findmycpu_ ()
 {
     int cpu;
