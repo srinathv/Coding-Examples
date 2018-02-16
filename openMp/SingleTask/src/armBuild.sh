f43fef45622fa47bd52feed7a3cb94587c4cda3e
Srinath Vadlamani <srinath.vadlamani@arm.com>
2018-02-15 20:01:32 -0700

    added for taskloop

--- openMp/SingleTask/src/armBuild.sh
+++ openMp/SingleTask/src/armBuild.sh
@@ -11,6 +11,7 @@ $FC $SRC $BASIC_FLAGS -o no_single_task.${TYPE}.exe
 $FC $SRC $BASIC_FLAGS -DSINGLE -o single_task.${TYPE}.exe
 $FC $SRC $BASIC_FLAGS -DSINGLE -DMORETASK -o single_moretask.${TYPE}.exe
 $FC $SRC $BASIC_FLAGS -DTASKLOOP -o no_single_taskloop.${TYPE}.exe
+$FC $SRC $BASIC_FLAGS -DSINGLE -DTASKLOOP -o single_taskloop.${TYPE}.exe
 
 echo " OMP_NUM_THREADS=5 ./no_single_task.${TYPE}.exe"
 OMP_NUM_THREADS=5 ./no_single_task.${TYPE}.exe
@@ -20,6 +21,8 @@ echo " OMP_NUM_THREADS=5 ./single_moretask.${TYPE}.exe"
 OMP_NUM_THREADS=5 ./single_moretask.${TYPE}.exe
 echo " OMP_NUM_THREADS=5 ./no_single_taskloop.${TYPE}.exe"
 OMP_NUM_THREADS=5 ./no_single_taskloop.${TYPE}.exe
+echo " OMP_NUM_THREADS=5 ./single_taskloop.${TYPE}.exe"
+OMP_NUM_THREADS=5 ./single_taskloop.${TYPE}.exe
 
 
 
