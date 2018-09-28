1c4fe3b4a7cbf4f17fd3887e9d9fb135b26f78a8
Srinath Vadlamani <srinath.vadlamani@arm.com>
2018-09-28 16:50:31 -0600

    better file name + test flag

--- pythonTools/testCompilerSpack.py
+++ pythonTools/testCompilerSpack.py
@@ -3,30 +3,30 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(description='Spack install test given compiler.')
-#parser.add_argument('integers', metavar='N', type=int, nargs='+',
-#                   help='an integer for the accumulator')
 parser.add_argument('compiler',help='give compiler syntax')
+parser.add_argument('--test',help='test with zip.zlib,zsh',action="store_true")
 
 args = parser.parse_args()
 
-print(args.compiler)
-fileName='spackInstall_' + args.compiler + '.txt'
+fileName='spackInstall_' + args.compiler.replace('@','_') + '.txt'
 
 import subprocess
 
-result = subprocess.check_output(['spack','list']).split()
-for package in result:
+allPackages = subprocess.check_output(['spack','list']).split()
+for package in allPackages:
     print package
 
 #subprocess.call(['spack','clone','${HOME}/spack-test'])
 subprocess.call(['spack','uninstall','--a','--y'])
 file = open(fileName,'w') 
 
+if args.test:
+    packageList=['zip','zlib','zsh']
+else:
+    packageList = allPackages
 
-testPlist=['zip','zlib','zsh']
-for testPackage in testPlist:
-    #tpwcompiler=testPackage+'%arm@18.4.1'
-    tpwcompiler=testPackage + '%' + args.compiler
+for package in packageList:
+    tpwcompiler = package + '%' + args.compiler
     result=subprocess.check_output(['spack','install','--fake',tpwcompiler]).split()
     didItInstall=result[2] + " is " + result[8]
     file.write(didItInstall + '\n')
