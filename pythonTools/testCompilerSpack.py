125f6578257b392e3f1b4730b9f5a7f3b926f018
Srinath Vadlamani <srinath.vadlamani@arm.com>
2018-09-28 15:47:16 -0600

    write result to file

--- pythonTools/testCompilerSpack.py
+++ pythonTools/testCompilerSpack.py
@@ -6,12 +6,23 @@ result = subprocess.check_output(['spack','list']).split()
 for package in result:
     print package
 
+#subprocess.call(['spack','clone','${HOME}/spack-test'])
+subprocess.call(['spack','uninstall','--a','--y'])
+file = open(“spackInstall_arm_18.4.1.txt”,”w”) 
+
+
 testPlist=['zip','zlib','zsh']
 for testPackage in testPlist:
-    tpwcompiler=testPackage+'%gcc@8.2.0'
-    print tpwcompiler
-#    print subprocess.check_output(['spack','install','--fake',tpwcompiler])
-    print subprocess.call(['spack','install','--fake',tpwcompiler])
+    tpwcompiler=testPackage+'%arm@18.4.1'
+#    print tpwcompiler
+    result=subprocess.check_output(['spack','install','--fake',tpwcompiler]).split()
+#    print result
+#    print result[2] + " is " + result[8]
+    didItInstall=result[2] + " is " + result[8]
+    file.write(didItInstall)
+    #print subprocess.call(['spack','install','--fake',tpwcompiler])
+
+file.close()
 def main():
     pass
 
