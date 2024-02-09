682a7d2f9eaf5986b4bc48116afff28e801283c1
Srinath Vadlamani <srinath.vadlamani@proton.me>
2024-02-09 06:57:00 -0700

    trying out assoiciate clause

---
+++ fortranAccociate/t2.f90
@@ -0,0 +1,30 @@
+!# from Gene Wagenbreth to test the associate clause
+
+
+
+
+
+  program t2
+    implicit none
+    integer:: i12 = 12
+    integer:: i = 1
+ 
+    print *,'start t2'
+ 
+    associate (i=>i12,j=>i12)
+      print *,'i=',i
+      print *,'j=',j
+    end associate
+ 
+    print *,'end t2'
+  end
+
+!ftn -g -O0 t2.F90 -o t2
+!./t2
+! start t2
+! i= 12
+! j= 12
+! end t2
+
+
+!# use a debugger to scope the 
