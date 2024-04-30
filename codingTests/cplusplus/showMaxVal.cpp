018cda3e5f6217f449693d8572fb2769b1f449ef
Srinath Vadlamani <srinath.vadlamani@hpe.com>
2024-04-29 19:49:52 -0500

    This is necessary to understand type values

--- codingTests/cplusplus/showMaxVal.cpp
+++ codingTests/cplusplus/showMaxVal.cpp
@@ -8,8 +8,12 @@ int main()
 int imin = std::numeric_limits<int>::min(); // minimum value
 int imax = std::numeric_limits<int>::max();
 
-std::cout<< "int min is "<< imin << "/n";
-std::cout << "int max is "<< imax << "/n";
+std::cout<< "int min is "<< imin << "\n";
+std::cout << "int max is "<< imax << "\n";
 
 
+int64_t imin64 = std::numeric_limits<int64_t>::min(); // minimum value
+int64_t imax64 = std::numeric_limits<int64_t>::max();
+std::cout<< "int64_t min is "<< imin64 << "\n";
+std::cout << "int64_t max is "<< imax64 << "\n";
 }
