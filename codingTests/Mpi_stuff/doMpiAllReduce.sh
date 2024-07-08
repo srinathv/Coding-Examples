039f9fc55dca378b767927c7785fa2b193f6b8f2
Srinath Vadlamani <srinath.vadlamani@hpe.com>
2024-07-07 18:46:24 -0700

    loop over with more pat directory logic

--- codingTests/Mpi_stuff/doMpiAllReduce.sh
+++ codingTests/Mpi_stuff/doMpiAllReduce.sh
@@ -1,17 +1,20 @@
 #! /usr/bin/env bash
 
 
+SOURCE_DIR=/usr/WS1/srinathv/REPO/Coding-Examples/codingTests/Mpi_stuff
 
-
-mpicc -o mpi_allreduce -O3 mpi_allreduce.c
-mpicxx -o mpiAllReduce -O3 mpiAllReduce.cc
+mpicc -o mpi_allreduce -O3 ${SOURCE_DIR}/mpi_allreduce.c
+mpicxx -o mpiAllReduce -O3 ${SOURCE_DIR}/mpiAllReduce.cc
 
 
 export MPICH_ENV_DISPLAY=1
 ml list
+flux --version 
 
-PROFILE=`pat_run -w -g mpi`
-
+PROFILE="pat_run -w -g mpi -- "
+#TIME="time "
+TRIAL=trial_3_wPatrun
+mkdir $TRIAL
 
 # need to loop over nodes then ranks
 #mpi250 ndoes vs mi300s all needs to be considered.
@@ -22,11 +25,15 @@ PROFILE=`pat_run -w -g mpi`
 NODE_SET=$(seq 1 16 1) #tiaga
 #NODE_SET={1..1000..10} #tiaga
 
-for j in $NODE_SET;
+for j in $(seq 1 16)
   do
+    export PAT_RT_EXPDIR_BASE=${PWD}/${TRIAL}/node_$j	  
     for i in $(seq 1 10);  
     do	
-      flux run -N $j -n $j --exclusive  $PROFILE ./mpiAllReduce 
+      output_file=${TRIAL}/mpiAllReduce_nodeNum_${j}_iteration_${i}.out.txt
+      date > $output_file
+      flux run -N $j -n $j --exclusive -o mpibind=verbose $TIME $PROFILE ./mpiAllReduce  &>> $output_file
+      date >> $output_file
     done
   done     
 
